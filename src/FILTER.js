ead0cee4e4a5387795512205d6832bd74e6cbebc
Peter Moresi <peter.moresi@gmail.com>
2015-11-03 20:57:38 -0800

    Update filter function

--- src/FILTER.js
+++ src/FILTER.js
@@ -1,8 +1,25 @@
-/* The filter an array or a range by a set of filters */ 
+/* The filter an array or a range by a set of filters */
+
 export default function FILTER(range, ...filters) {
   // TBD: Need to filter range by filters.
   // A filter may be either a function or an array of true/false values.
   // The filter may be for rows or for columns but not both.
   // A array filter may only filter a range that covers a single row or a single column.
+
+  function makeFilter() {
+    return (value, index) => {
+      return filters.reduce( (previousValue, currentValue, filterIndex) => {
+        if (previousValue === false ) {
+          return false;
+        } else if (previousValue === true) {
+          return currentValue[index];
+        } else {
+          return previousValue[index] && currentValue[index]
+        }
+      });
+    }
+  }
+
+  return range.filter( makeFilter() )
 
 }
--- test/FILTER.js
+++ test/FILTER.js
@@ -1,6 +1,15 @@
-var assert = require('assert');
+/* FILTER a dataset by reduction
+ *
+ */
 
-describe('FILTER', function() {
-  it('should requires a range and at least one filter', function() {
-  });
+import test from 'tape';
+import filter from '../src/FILTER'
+
+test('FILTER', (t) => {
+  t.plan(3)
+  t.deepEqual( filter( [[1], [2]], [true, false], [true, true] ), [[1]] )
+  t.deepEqual( filter( [[1], [2]], [false, true], [true, true] ), [[2]] )
+  t.deepEqual( filter( [[1, 1], [2, 2], [3, 3]],
+    [false, true, true], [true, true, true] ),
+    [[2, 2], [3, 3]] )
 });
